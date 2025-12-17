ad88616554239cd21bc4a461eb6aaf4145b2676f
Rafael Alexander Reymundo Serpa <rafaelreymundoserpa@gmail.com>
2025-12-17 11:31:02 -0500

    Initial commit

---
+++ src/main/java/pe/com/practicar/MsZonasSegurasApplication.java
@@ -0,0 +1,13 @@
+package pe.com.practicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsZonasSegurasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsZonasSegurasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/practicar/MsZonasSegurasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.practicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsZonasSegurasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
