19baf4eba8c7a9573d826a1426a2ac2b210fb422
Rafael Alexander Reymundo Serpa <rafaelreymundoserpa@gmail.com>
2026-02-09 15:00:33 -0500

    [feature] Correccion Test Sonar

--- src/test/java/pe/com/practicar/business/validator/ZoneCreateRequestValidatorTest.java
+++ src/test/java/pe/com/practicar/business/validator/ZoneCreateRequestValidatorTest.java
@@ -1,6 +1,8 @@
 package pe.com.practicar.business.validator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import pe.com.practicar.business.exception.BusinessException;
 import pe.com.practicar.expose.schema.ZoneDatosCreateRequest;
 import reactor.test.StepVerifier;
@@ -97,21 +99,6 @@ class ZoneCreateRequestValidatorTest {
     }
 
     @Test
-    void validate_ConNombreConCaracteresEspeciales_DeberiaLanzarExcepcion() {
-        ZoneDatosCreateRequest request = new ZoneDatosCreateRequest();
-        request.setCodzona(1);
-        request.setNombre("Zona@#$%");
-        request.setLatitud(-12.1191);
-        request.setLongitud(-77.0292);
-        request.setNivelSeguridad(5);
-        request.setUsuarioCreacion("admin");
-
-        StepVerifier.create(ZoneCreateRequestValidator.validate(request))
-                .expectError(BusinessException.class)
-                .verify();
-    }
-
-    @Test
     void validate_ConNombreMuyLargo_DeberiaLanzarExcepcion() {
         ZoneDatosCreateRequest request = new ZoneDatosCreateRequest();
         request.setCodzona(1);
@@ -126,14 +113,20 @@ class ZoneCreateRequestValidatorTest {
                 .verify();
     }
 
-    @Test
-    void validate_ConLatitudFueraDeRango_DeberiaLanzarExcepcion() {
+    @ParameterizedTest(name = "{0}")
+    @CsvSource({
+        "Nombre con caracteres especiales, Zona@#$%, -12.1191, -77.0292, 5",
+        "Latitud fuera de rango, Zona Centro Lima, -91.0, -77.0292, 5",
+        "Nivel seguridad menor a 1, Zona Centro Lima, -12.1191, -77.0292, 0",
+        "Nivel seguridad mayor a 10, Zona Centro Lima, -12.1191, -77.0292, 11"
+    })
+    void validate_ConDatosInvalidos_DeberiaLanzarExcepcion(String escenario, String nombre, double latitud, double longitud, int nivelSeguridad) {
         ZoneDatosCreateRequest request = new ZoneDatosCreateRequest();
         request.setCodzona(1);
-        request.setNombre("Zona Centro Lima");
-        request.setLatitud(-91.0);
-        request.setLongitud(-77.0292);
-        request.setNivelSeguridad(5);
+        request.setNombre(nombre);
+        request.setLatitud(latitud);
+        request.setLongitud(longitud);
+        request.setNivelSeguridad(nivelSeguridad);
         request.setUsuarioCreacion("admin");
 
         StepVerifier.create(ZoneCreateRequestValidator.validate(request))
@@ -149,36 +142,6 @@ class ZoneCreateRequestValidatorTest {
         request.setLatitud(-12.1191);
         request.setLongitud(181.0);
         request.setNivelSeguridad(5);
-        request.setUsuarioCreacion("admin");
-
-        StepVerifier.create(ZoneCreateRequestValidator.validate(request))
-                .expectError(BusinessException.class)
-                .verify();
-    }
-
-    @Test
-    void validate_ConNivelSeguridadMenorAUno_DeberiaLanzarExcepcion() {
-        ZoneDatosCreateRequest request = new ZoneDatosCreateRequest();
-        request.setCodzona(1);
-        request.setNombre("Zona Centro Lima");
-        request.setLatitud(-12.1191);
-        request.setLongitud(-77.0292);
-        request.setNivelSeguridad(0);
-        request.setUsuarioCreacion("admin");
-
-        StepVerifier.create(ZoneCreateRequestValidator.validate(request))
-                .expectError(BusinessException.class)
-                .verify();
-    }
-
-    @Test
-    void validate_ConNivelSeguridadMayorADiez_DeberiaLanzarExcepcion() {
-        ZoneDatosCreateRequest request = new ZoneDatosCreateRequest();
-        request.setCodzona(1);
-        request.setNombre("Zona Centro Lima");
-        request.setLatitud(-12.1191);
-        request.setLongitud(-77.0292);
-        request.setNivelSeguridad(11);
         request.setUsuarioCreacion("admin");
 
         StepVerifier.create(ZoneCreateRequestValidator.validate(request))
