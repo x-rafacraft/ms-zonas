97bb22ec80e6861e5b775df0f72d6c66209bbe2b
Rafael Alexander Reymundo Serpa <rafaelreymundoserpa@gmail.com>
2026-03-05 22:31:09 -0500

    [feat] cambio nomenclatura de ciudad a city

--- src/main/java/pe/com/practicar/expose/controller/ZonesApi.java
+++ src/main/java/pe/com/practicar/expose/controller/ZonesApi.java
@@ -24,7 +24,7 @@ public interface ZonesApi {
             @RequestParam(required = false) String provincia,
             @RequestParam(required = false) String distrito,
             @RequestParam(required = false) Integer nivelSeguridad,
-            @RequestParam(required = false) String ciudad,
+            @RequestParam(value = "city", required = false) String ciudad,
             @RequestParam(required = false) String minRisk,
             @RequestParam(required = false) String maxRisk,
             final ServerWebExchange exchange);
